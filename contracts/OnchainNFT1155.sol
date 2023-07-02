3fcb750398f628f3ba83e3b12e6485d8ac832aa0
Simon Ionashku <simon.ionashku@gmail.com>
2023-07-02 15:29:04 -0400

    [00] - Initial commit

---
+++ contracts/OnchainNFT1155.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+
+contract OnchainNFT1155 is ERC1155, ERC1155Burnable, Ownable, ERC1155Supply {
+    uint256 public constant AIMO = 0;
+    uint256 public constant maxAIMOsupply = 10000;
+
+    uint256 public constant bravoMAXsuppy = 100;
+
+    constructor() ERC1155("") {
+        _mint(msg.sender, AIMO, 10 ** 4, "");
+    }
+
+    function mint(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public onlyOwner {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public onlyOwner {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal override(ERC1155, ERC1155Supply) {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    }
+}
+
+//Substract _amount
+//   balances[_from][_id] = balances[_from][_id].sub(_amount);
