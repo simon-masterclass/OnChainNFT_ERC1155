953f62925ba994c9159f8066a7083b74e18926d4
Simon Ionashku <simon.ionashku@gmail.com>
2023-07-04 12:46:55 -0400

    [03] - AIM0wner minting

--- contracts/OnchainNFT1155.sol
+++ contracts/OnchainNFT1155.sol
@@ -7,32 +7,33 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 
 contract OnchainNFT1155 is ERC1155, ERC1155Burnable, Ownable, ERC1155Supply {
-    address public AIM0wner = address(this);
+    address public AIM0wner;
     uint256 public constant AIM0 = 0;
     uint256 public constant AIM0bonus = 100;
-    uint256 public constant maxAIM0supply = 10000;
+    uint256 public constant maxAIM0supply = 10 ** 6;
     uint256 public constant maxBRAVOsupply = 100;
 
     uint256[] public bravoIDs;
     string[] public bravoCodeNames;
 
     constructor() ERC1155("") {
-        _mint(AIM0wner, AIM0, maxAIM0supply, "");
+        AIM0wner = msg.sender;
+        _mint(AIM0wner, AIM0, maxAIM0supply - 10000, "");
         bravoIDs.push(AIM0);
-        bravoCodeNames.push("C0");
+        bravoCodeNames.push("$AIMO");
     }
 
     function mint(string memory codeName) public {
-        require(
-            bravoIDs.length <= maxBRAVOsupply,
-            "Max supply of Bravo NFTs reached"
-        );
+        uint256 newID = bravoIDs.length;
+        require(newID <= maxBRAVOsupply, "Max supply of Bravo NFTs reached");
+        require(totalSupply(0) < maxAIM0supply, "Max supply of $AIM0 reached");
         //mint new soul bound NFT for Bravo company recruit with code name
-        _mint(msg.sender, bravoIDs.length, 1, "");
-        bravoIDs.push(bravoIDs.length);
+        _mint(msg.sender, newID, 1, "");
+        bravoIDs.push(newID);
         bravoCodeNames.push(codeName);
         //transfer 100 rounds of AIM0 to the new recruit as enlistment bonus
-        _safeTransferFrom(AIM0wner, msg.sender, AIM0, AIM0bonus, "");
+        //_safeTransferFrom(AIM0wner, msg.sender, AIM0, AIM0bonus, "");
+        _mint(msg.sender, AIM0, AIM0bonus, "");
     }
 
     function mintBatch(
@@ -55,9 +56,5 @@ contract OnchainNFT1155 is ERC1155, ERC1155Burnable, Ownable, ERC1155Supply {
         bytes memory data
     ) internal override(ERC1155, ERC1155Supply) {
         super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
-        require(
-            to == address(0) || msg.sender == AIM0wner,
-            "Tokens are soul bound & cannot be transferred, only burned."
-        );
     }
 }
